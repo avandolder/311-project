b94bc98fa14628be241f743046494dc0609851aa
Adam Vandolder <adam.vandolder@gmail.com>
2018-11-20 17:05:59 -0500

    Added Course, CourseTime, and Professor models

--- finder/models.py
+++ finder/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class CourseTime(models.Models):
+    dates = models.CharField(max_length=3)
+    start_time = models.IntegerField()
+    end_time = models.IntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Professor(models.Models):
+    name = models.CharFiel(max_length=50)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Course(models.Models):
+    faculty = models.IntegerField()
+    department = models.IntegerField()
+    course = models.IntegerField()
+    name = models.CharField(max_length=50)
+    desc = models.TextField()
+    review_amt = models.IntegerField()
+    review_avg = models.FloatField(min=1.0, max=5.0)

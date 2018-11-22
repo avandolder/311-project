b0bff1fd8be990c2810bb2e050a439d4fe1c3213
Adam Vandolder <adam.vandolder@gmail.com>
2018-11-22 16:56:19 -0500

    Added models to admin editor

--- finder/admin.py
+++ finder/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, CourseTime, Professor
+
+admin.site.register(Course)
+admin.site.register(CourseTime)
+admin.site.register(Professor)

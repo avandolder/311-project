4d95a6b8fe09daca84d2eef67cebb85c51c69e9d
root <root@DESKTOP-8HJ0L1G.localdomain>
2018-11-25 17:29:44 -0500

    Added signup view

--- CourseFinder/urls.py
+++ CourseFinder/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
+from finder import views
 
 urlpatterns = [
     path('finder/', include('finder.urls')),
     path('admin/', admin.site.urls),
+    url('signup/', views.signup)
 ]
--- finder/views.py
+++ finder/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
+def signup(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data_.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			login(request, user)
+			return #redirect('home') uncomment once we have a home page
+	else:
+		form = UserCreationForm()
+	return render(request, 'finder/signup.html', {'form':form})
 
 def index(request):
     return render(request, 'finder/index.html', {})

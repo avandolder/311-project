0ee541dc3420483d5460a069ed67f32af376fe71
Adam Vandolder <adam.vandolder@gmail.com>
2018-11-25 17:52:37 -0500

    Add semesters as a field to the CourseTime model

--- finder/models.py
+++ finder/models.py
@@ -16,10 +16,11 @@ class CourseTime(models.Model):
     dates = models.CharField(max_length=3)
     start_time = models.IntegerField()
     end_time = models.IntegerField()
+    semesters = models.TextField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.course.name}: {self.dates} {self.start_time}-{self.end_time}'
+        return f'{self.course.name}: {self.semesters} {self.dates} {self.start_time}-{self.end_time}'
 
 class Professor(models.Model):
     name = models.CharField(max_length=50)

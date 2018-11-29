ae9e793aa5d7b3d9703829ac1c0ecc97289aa2f2
Adam Vandolder <adam.vandolder@gmail.com>
2018-11-28 20:19:44 -0500

    Added searching for course by code or name (only works for 1 course)

--- finder/views.py
+++ finder/views.py
@@ -8,42 +8,54 @@ from django.views.generic.detail import DetailView
 from finder.models import Course
 
 def signup(request):
-	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data.get('username')
-			raw_password = form.cleaned_data.get('password1')
-			user = authenticate(username=username, password=raw_password)
-			login(request, user)
-			return redirect('/finder')
-	else:
-		form = UserCreationForm()
-	return render(request, 'finder/signup.html', {'form':form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/finder')
+    else:
+        form = UserCreationForm()
+    return render(request, 'finder/signup.html', {'form':form})
 
 def index(request):
     return render(request, 'finder/index.html', {})
 
 def logout(request):
-	django_logout(request)
-	return redirect('/login')
+    django_logout(request)
+    return redirect('/login')
 
 def profile(request):
-	return render(request, 'finder/profile.html')
+    return render(request, 'finder/profile.html')
 
 def search(request):
-	if request.method == 'GET':
-		search_query = request.GET.get('search_box', None)
-		#query course database
-	return render(request, 'finder/search.html')
+    if request.method == 'GET':
+        search_query = request.GET.get('search_box', None)
+        if not search_query:
+            return render(request, 'finder/search.html')
+
+        # Check if query is by name or code.
+        if search_query.lstrip()[1].isdigit():
+            # If the first character is numeric, then search by code.
+            return redirect('/courseinfo/' + search_query.strip())
+        else:
+            courses = Course.objects.filter(name__icontains=search_query)
+            print(courses)
+            if len(courses) == 1:
+                course = courses[0]
+                course_code = f'{course.faculty}-{course.department}-{course.course}'
+                return redirect('/courseinfo/' + course_code)
 
 def faqs(request):
-	return render(request, 'finder/faqs.html')
+    return render(request, 'finder/faqs.html')
 
 def paths(request):
-	#return users saved paths
-	return render(request, 'finder/paths.html')
+    #return users saved paths
+    return render(request, 'finder/paths.html')
 
 def courseinfo(request, faculty, department, course):
-	course = get_object_or_404(Course, faculty=faculty, department=department, course=course)
-	return render(request, 'finder/courseinfo.html', {'course': course})
+    course = get_object_or_404(Course, faculty=faculty, department=department, course=course)
+    return render(request, 'finder/courseinfo.html', {'course': course})
